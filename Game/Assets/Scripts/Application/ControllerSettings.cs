4d421b62b3c666efe29ab00b5f1e7ce8195d703f
Swen <swenmeeu@gmail.com>
2018-08-11 01:02:18 +0200

    Implemented Controller Settings

---
+++ Game/Assets/Scripts/Application/ControllerSettings.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "Controller Settings", menuName = "Settings/Controller Settings")]
+public class ControllerSettings : ScriptableObject
+{
+    public float DeadZone;
+}
