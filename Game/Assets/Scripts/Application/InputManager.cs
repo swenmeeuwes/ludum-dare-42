e89c96ccfc8139b53d472fd53216e7f101d64297
Swen <swenmeeu@gmail.com>
2018-08-11 16:13:08 +0200

    Patched Input Manager

--- Game/Assets/Scripts/Application/InputManager.cs
+++ Game/Assets/Scripts/Application/InputManager.cs
@@ -64,6 +64,19 @@ public class InputManager : ITickable
 
     private bool CheckKeyboardAndMouse()
     {
-        return Input.anyKey; // Triggers when some buttons on the controller are pressed        
+        //return Input.anyKey; // Triggers when some buttons on the controller are pressed
+        return Input.GetKey(KeyCode.W) ||
+               Input.GetKey(KeyCode.S) ||
+               Input.GetKey(KeyCode.A) ||
+               Input.GetKey(KeyCode.D) ||
+               Input.GetKey(KeyCode.KeypadEnter) ||
+               Input.GetKey(KeyCode.Space) ||
+               Input.GetKey(KeyCode.UpArrow) ||
+               Input.GetKey(KeyCode.DownArrow) ||
+               Input.GetKey(KeyCode.LeftArrow) ||
+               Input.GetKey(KeyCode.RightArrow) ||
+               Input.GetMouseButton(0) ||
+               Input.GetMouseButton(1) ||
+               Input.GetMouseButton(2);
     }
 }
