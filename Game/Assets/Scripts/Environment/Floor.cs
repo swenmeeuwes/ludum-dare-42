d927a0c3e572172aafa2b4d48dc5247fa8fddb30
Swen <swenmeeu@gmail.com>
2018-08-11 19:07:49 +0200

    Made the floor

---
+++ Game/Assets/Scripts/Environment/Floor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class Floor : MonoBehaviour
+{
+    private Animator _animator;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Used in animation
+    public void OnDarkenAnimationFinished()
+    {
+        
+    }
+}
