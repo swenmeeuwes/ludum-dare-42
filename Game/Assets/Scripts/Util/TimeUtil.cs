cf2655d5094ca7b9c07ba6af41df19a9a8d87bc9
Swen <swenmeeu@gmail.com>
2018-08-13 16:25:40 +0200

    Implemented time util

---
+++ Game/Assets/Scripts/Util/TimeUtil.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class TimeUtil
+{
+    public static string SecondsToDigitalClock(int seconds)
+    {
+        var mm = Mathf.FloorToInt(seconds / 60f).ToString("D2");
+        var ss = (seconds % 60).ToString("D2");
+
+        return string.Format("{0}:{1}", mm, ss);
+    }
+
+    public static string SecondsToDigitalClock(float seconds)
+    {
+        return SecondsToDigitalClock(Mathf.RoundToInt(seconds));
+    }
+}
