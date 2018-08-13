f3d3985e66645d6546ed84aa63b04caf352fe5d8
Swen <swenmeeu@gmail.com>
2018-08-13 13:40:33 +0200

    Added selection arrow buttons

---
+++ Game/Assets/Scripts/UI/ButtonSelectionArrows.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonSelectionArrows : Button
+{
+    private SelectionArrow[] _selectionArrows;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        _selectionArrows = GetComponentsInChildren<SelectionArrow>(true);
+
+        SetArrowsActive(false);
+    }
+
+    public override void OnSelect(BaseEventData eventData)
+    {
+        base.OnSelect(eventData);
+
+        SetArrowsActive(true);
+    }
+
+    public override void OnDeselect(BaseEventData eventData)
+    {
+        base.OnDeselect(eventData);
+
+        SetArrowsActive(false);
+    }
+
+    private void SetArrowsActive(bool active)
+    {
+        foreach (var arrow in _selectionArrows)
+        {
+            arrow.gameObject.SetActive(active);
+        }
+    }
+}
---
+++ Game/Assets/Scripts/UI/SelectionArrow.cs
@@ -0,0 +1,6 @@
+﻿using UnityEngine;
+
+// Quick hack for 'ButtonSelectionArrow', Button has a custom inspector which does not expose public field in 'ButtonSelectionArrow'
+public class SelectionArrow : MonoBehaviour {
+    
+}
