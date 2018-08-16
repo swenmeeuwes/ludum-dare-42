c505ae13d580c25ce5614cf5a8165fc7ff81f6cb
Swen <swenmeeu@gmail.com>
2018-08-16 16:52:25 +0200

    Added background music

---
+++ Game/Assets/Scripts/Audio/BackgroundMusicManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using Zenject;
+
+[RequireComponent(typeof(ZenjectBinding), typeof(AudioSource))]
+public class BackgroundMusicManager : MonoBehaviour
+{
+    private AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        DontDestroyOnLoad(gameObject);
+    }
+}
